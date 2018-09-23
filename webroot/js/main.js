379c50eb9c571018e596ed107b4e3e3cb3fbb17f
juanmagismondi <43192978+juanmagismondi@users.noreply.github.com>
2018-09-23 17:36:12 -0300

    inicializo mi branch

---
+++ webroot/js/main.js
@@ -0,0 +1,6 @@
+document.getElementById('toggle').addEventListener('click', function() {
+	var form = document.getElementById('test')
+	var from2 = document.getElementById('test2')
+	form.classList.add('asd')
+	from2.classList.add('asd2')
+})
