d21e0083445c8841609b4eeafd0b7465901336b4
ArielOrrino <arielorrino@hotmail.com>
2018-09-23 17:59:09 -0300

    login merged

--- webroot/index.php
+++ webroot/index.php
@@ -1,42 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Login UtnCoop</title>
-	<link rel="stylesheet" href="css/estilos.css">
-</head>
-<body>
-	<div class="contenedor-form" id='toggle'>
-		<div class="toggle">
-		    <span>Crear Cuenta</span>	
-		</div>
-		<div class="formulario session" id='test'>
-			<h2>Iniciar Sesión</h2>
-			<form action="#">
-				<input type="text" placeholder="Usuario" required>
-				<input type="password" placeholder="Contraseña" required>
-				<input type="submit" value="Iniciar Sesión">
-			</form>
-		</div>
+<?php
+/**
+ * The Front Controller for handling every request
+ *
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         0.2.9
+ * @license       MIT License (https://opensource.org/licenses/mit-license.php)
+ */
 
-		<div class="formulario create" id='test2'>
-			<h2>Crea tu Cuenta</h2>
-			<form action="#">
-				<input type="text" placeholder="Usuario" required>
-				<input type="password" placeholder="Contraseña" required>
-				<input type="email" placeholder="Correo Electrónico" required>
-				<input type="text" placeholder="Celular" required>
-				<input type="submit" value="Registrarse">
-			</form>
-		</div>
+// Check platform requirements
+require dirname(__DIR__) . '/config/requirements.php';
 
-		<div class="reset-password">
-			<a href="#">Olvide mi contraseña?</a>
-		</div>
-    </div>
-     
-     <script src="js/jquery-3.3.1.min.js"></script>
-     <script src="js/main.js"></script>
+// For built-in server
+if (PHP_SAPI === 'cli-server') {
+    $_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
 
-</body>
-</html>
+    $url = parse_url(urldecode($_SERVER['REQUEST_URI']));
+    $file = __DIR__ . $url['path'];
+    if (strpos($url['path'], '..') === false && strpos($url['path'], '.') !== false && is_file($file)) {
+        return false;
+    }
+}
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+use App\Application;
+use Cake\Http\Server;
+
+// Bind your application to the server.
+$server = new Server(new Application(dirname(__DIR__) . '/config'));
+
+// Run the request/response through the application and emit the response.
+$server->emit($server->run());
